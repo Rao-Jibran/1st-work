86b37e6fc8f3dcc3466c2fd598a95f6794995101
Rao-Jibran <raojibrankhan@gmail.com>
2021-02-10 22:06:27 +0500

    work

---
+++ index.js
@@ -0,0 +1,382 @@
+// var first="jibran"
+// var last="khan"
+// console.log(first)
+
+
+// var userNum1=+prompt("enter number 1 :")
+// var userNum2=+prompt("enter number 2")
+// var result= (userNum1 + userNum2)
+
+// console.log(result)
+
+// var mark1=+prompt("marks in math")
+// var mark2=+prompt("marks in english")
+// var mark3=+prompt("marks in urdu")
+
+// var totalMarks= 300
+// var marksObtained= mark1+mark2+mark3 
+
+// var percentage= (marksObtained/300) * 100 
+// console.log("marks in math "+mark1)
+// console.log("marks in english "+mark2)
+// console.log("marks in urdu "+mark3)
+// console.log("percentage is "+percentage+"%")
+
+
+
+// var  num1=+prompt("enter number 1")
+// var num2=+prompt("enter number 2")
+// var result=(num1 + num2)
+// alert("sum of "+num1+ "and" +num2+ "is" +result)
+
+
+
+// var num1=600
+// var num2=+prompt("enter number of tickets : ")
+// var result=(num1*num2)
+// alert ("your total price for: " +num2+ " tickets is : " +result)
+
+
+// >>>>>>>>>>>>>>>>>>>>>>SUPER MART<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var num1=600
+// var num2=400
+// var num3=150
+// var num4=+prompt("enter quantity of item 1 : ")
+// var num5=+prompt("enter quantity of item 2 : ")
+// var result1=num1*num4
+// var result2=num2*num5
+// var result3=num3+result1+result2
+// console.log("quantity of item 1 is : " +num4)
+// console.log("quantity of item 2 is : " +num5)
+// console.log("price of item 1 is : " +result1)
+// console.log("price of item 2 is : " +result2)
+// console.log(" ")
+// console.log("shipping charges is "+num3)
+// console.log(" ")
+// console.log("total cost of your order is : "+result3)
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>TABLE<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var table=+prompt("Enter number")
+// console.log(table+" X "+1+" = "+table*1)
+// console.log(table+" X "+2+" = "+table*2)
+// console.log(table+" X "+3+" = "+table*3)
+// console.log(table+" X "+4+" = "+table*4)
+// console.log(table+" X "+5+" = "+table*5)
+// console.log(table+" X "+6+" = "+table*6)
+// console.log(table+" X "+7+" = "+table*7)
+// console.log(table+" X "+8+" = "+table*8)
+// console.log(table+" X "+9+" = "+table*9)
+// console.log(table+" X "+10+" = "+table*10)
+
+
+// >>>>>>>>>>>>>>>>>>>>>TEMP CONVERSION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var temp1=+prompt("enter temp in celcius")
+// var temp2=+prompt("enter temp in farenheit")
+// var result1=(temp1-32)* (0.555)
+// var result2=(temp2*9/5)+32
+// console.log("temperature in Farenheit is : " +result1)
+// console.log("temperature in Celcius is : " +result2)
+
+
+// >>>>>>>>>>>>>>>>>>>>>>MARK SHEET<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,
+// var mark1=+prompt("enter marks in maths : ")
+// var mark2=+prompt("enter marks in english : ")
+// var mark3=+prompt("enter marks in urdu : ")
+// var totalMarks=300
+// var marksObtained=mark1+mark2+mark3
+// var percentage=(marksObtained/totalMarks) *100
+// console.log("total marks :" +300)
+// console.log("marks obtained :" +marksObtained)
+// console.log("percentage :"+ +percentage+ "%")
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>CURRENCY EXCHANGE<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var dollars=10
+// var riyal=25
+// var rupee1=10*104
+// var rupee2=25*28
+// var result=rupee1+rupee2
+// console.log("CURRENCY IN PKR")
+// console.log("total currency in pkr is : " +result)
+
+
+// >>>>>>>>>>>>>>>>>AGE CALCULATOR<<<<<<<<<<<<<<<<<<<<<<<<<
+// var birthYear=+prompt("enter you birth year")
+// var currentYear=2021
+// var age=currentYear-birthYear
+// console.log("THE AGE CALCULATOR")
+// console.log("current year : " +currentYear)
+// console.log("birth year : " +birthYear)
+// console.log("your age is : " +age)
+
+
+// >>>>>>>>>>>>>>>>>>>>>The Geometrizer<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var radius=+prompt("enter radius of circle : ")
+// var circumference=2*3.142*radius
+// var area=3.142*(radius*radius)
+// console.log("GEOMETRIZER")
+// console.log("radius of circle : " +radius)
+// console.log("the circumference is : " +circumference)
+// console.log("the area is : " +area)
+
+
+
+
+// >>>>>>>>>>>>>>>>IF ELSE<<<<<<<<<<<<<<<<<<<<<
+// var temp1=+prompt("enter temp in celcius")
+// var temp2=+prompt("enter temp in farenheit")
+// var result1=(temp1-32)* (0.555)
+// var result2=(temp2*9/5)+32
+// if (temp1===12)
+// {
+// console.log("true")
+// }
+// else
+// {
+// console.log("false")
+// }
+
+
+// >>>>>>>>>>>>>>>>IF ELSE MARK SHEET<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var mark1=+prompt("enter marks in maths : ")
+// var mark2=+prompt("enter marks in english : ")
+// var mark3=+prompt("enter marks in urdu : ")
+// var totalMarks=300
+// var marksObtained=mark1+mark2+mark3
+// var percentage=(marksObtained/totalMarks) *100
+// console.log("total marks :" +300)
+// console.log("marks obtained :" +marksObtained)
+// console.log("percentage :"+ +percentage+ "%")
+// if (percentage>70)
+// {
+//     console.log("GREAT")
+// }
+// else if (percentage>=50 &&  percentage <=70 )
+// {
+//     console.log("AVERAGE")
+// }
+// else 
+// {
+//     console.log("FAIL")
+// }
+
+
+// >>>>>>>>>>>>>>>>>>>SWITCH CASE<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var num=+prompt("enter number : ")
+// switch (num) 
+// {
+//     case 1 :
+//         alert("JANUARY")
+//         break
+//         case 2 :
+//             alert("FEBUARY")
+//             break
+//             case 3 :
+//                 alert("MARCH")
+//                 break
+//                 case 4 :
+//                     alert("APRIL")
+//                     break 
+//                     default :
+//                     alert("ERROR 404")
+//   }
+  
+
+
+// >>>>>>>>>>>>>>>>>>>>>EMAIL<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var email=prompt("enter your email")
+// var pass=prompt("enter your password")
+//  if (email==="raojibran@gmail.com" && pass==="12345") 
+// {
+//     console.log("verified")
+// }
+// else 
+// {
+//     console.log("ACCESS DENIED")
+// }
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>ARRAY<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// var names=[]
+// var enter1=prompt("enter name 1 : ")
+// var enter2=prompt("enter name 2 : ")
+// var enter3=prompt("enter name 3 : ")
+// var enter4=prompt("enter name 4 : ")
+
+// names.push(enter1)
+// names.push(enter2)
+// names.push(enter3)
+
+// console.log(names)
+
+// names.unshift(enter4)
+
+
+
+// >>>>>>>>>>>>>>>>>>ARRAY PRACTICS <<<<<<<<<<<<<<<<<<<<<
+
+// var cars=["audi","volvo","ford","lamborgini"]
+// console.log("first index of array : 0 ")
+// console.log("car at the first index of the array : " +cars[0])
+// console.log("last index of the array : 3 ")
+// console.log("car at last index of array : " +cars[3])
+
+
+
+// >>>>>>>>>>>>>>>>>>>splice<<<<<<<<<<<<<<<<<<<<<<<<<
+// var cities=["karachi","lahore","queta","multan","peshawar"]
+// cities.splice(2,1,"islamabad")
+// console.log(cities)
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>Q1<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// var num1=+prompt("enter number : ")
+// var cal=(num1 % 3===0)
+// if (cal)
+// {
+//     console.log("divisible by 3 ")
+// }
+// else{
+//     console.log("not divisible by 3  ")
+// }
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>Q2<<<<<<<<<<<<<<<<<<<<<<<
+// var num1=+prompt("enter number")
+// var cal=(num1 % 2===0)
+// if (cal)
+// {
+//     console.log("even number")
+// }
+// else {
+//     console.log("odd number ")
+// }
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Q3<<<<<<<<<<<<<<<<<<<<<<<<<
+// var num1=+prompt("enter you age")
+// var ageO=(num1 >= 18)
+// if (ageO)
+// {
+//     alert("access granted")
+// }
+// else {
+//     alert("TOO YOUNG FOR THIS")
+// }
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>Q4<<<<<<<<<<<<<<<<<<<<<<<<
+// var enter=prompt("enter your name")
+// var names=["hammad","jibran","ali","ahmed"]
+// if (enter===names[0])
+// {
+//     alert("WELCOME to site")
+// }
+// else if (enter===names[1])
+// {
+//     alert("WELCOME to site")
+// }
+// else if (enter===names[2])
+// {
+//     alert("WELCOME to site")
+// }
+// else if (enter===names[3])
+// {
+//     alert("WELCOME to site")
+// }
+// else {
+//     alert("you are not allowed to access")
+// }
+
+ 
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Q7<<<<<<<<<<<<<<<<<<<<<<
+// var num1=+prompt("enter number ")
+// var sign=prompt("enter your operator ")
+// var num2=+prompt("enter second number ")
+// switch (sign)
+// {
+//     case ("+"):
+//     alert (num1 + num2)
+//     break
+
+//     case ("-") :
+//         alert (num1 - num2)
+//         break
+
+//         case ("*")  :
+//             alert (num1 * num2)
+//             break
+
+//             case ("/") :
+//                 alert (num1 / num2)
+//                 break
+// }
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>Q8<<<<<<<<<<<<<<<<<<<<<<<
+// var time=+prompt("enter time : ")
+// if (time>=1 && time<3)
+// {
+//     alert("GOOD MORNING")
+// }
+// else if (time>=3 && time<5)
+// {
+//     alert("good afternoon")
+// }
+// else if (time>=5 && time<7)
+// {
+//     alert("good evening")
+// }
+// else if (time>=7 && time<=12)
+// {
+//     alert("good night ")
+// }
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>Q9<<<<<<<<<<<<<<<<<<<<<<<<<
+// var year=+prompt("enter year : ")
+// if (year + 4 )
+// {
+//     alert("leap year")
+// }
+// else if (year - 4)
+// {
+//     alert("leap year")
+// }
+// else {
+//     alert("not leap year")
+// }
+
+
+
+
+// >>>>>>>>>>>>>>>>>>Q10<<<<<<<<<<<<<<<<<<<<<
+// var pass="12345"
+// var pass2=""
+// var enter=prompt("enter password")
+// if (enter===pass)
+// {
+//     alert("correct password")
+// }
+// else if (enter===pass2)
+// {
+//     alert("enter password")
+// }
+// else
+// {
+//     alert("password is incorrect")
+// }
